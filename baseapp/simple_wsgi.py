bb06e00494e700f1db4fc73da89ac1d768df664f
Zephrys_TheGreatest <froloff1830@gmail.com>
2024-12-24 20:23:28 +0300

    [UPDATE] | final MVP version for deploy (сча поиск допишу и норм будет)

--- baseapp/simple_wsgi.py
+++ baseapp/simple_wsgi.py
@@ -1,7 +1,10 @@
+import os
+import sys
+from django.core.wsgi import get_wsgi_application
 from waitress import serve
 
 
-def application(environ, start_response):
+def simple_application(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
     start_response(status, headers)
@@ -18,5 +21,13 @@ def application(environ, start_response):
     return [response_body.encode('utf-8')]
 
 
-if __name__ == "__main__":
+def main_application():
+    sys.path.append('A:/PycharmProjects/askme_frolov/baseapp')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baseapp.settings')
+
+    application = get_wsgi_application()
     serve(application, host='0.0.0.0', port=8081)
+
+
+if __name__ == "__main__":
+    main_application()
